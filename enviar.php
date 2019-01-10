297ea5041bf079e226205cc1160a6358bec03c9c
William Franco <Zaethar@users.noreply.github.com>
2019-01-10 17:13:11 -0300

    Actualizacion
    Se añadieron enlaces en los iconos de las redes sociales que redirigen a sus respectivas paginas, nuevos colores agregados a los botones "enviar" y el active de Servicios ZT, se elimino seccion about, se añadieron datos en seccion Contacto y el boton enviar, manda un email con los datos al ser presionado, se añadieron datos en Servicios ZT(viandas, postres y bebidas)

---
+++ enviar.php
@@ -0,0 +1,9 @@
+<?php
+ $destino= "Skullfitzarza@gmail.com";
+ $nombre = $_POST["name"];
+ $correo=$_POST["email"];
+ $mensaje=$_POST["message"];
+ $contenido="Nombre: " . $nombre . "\nCorreo: " . $correo . "\nMensaje: " . $mensaje;
+ mail($destino,"Contacto", $contenido);
+ header("Location:index.html");
+?>
